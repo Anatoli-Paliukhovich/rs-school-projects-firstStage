f0389348f65c0cc363488c29659fcfb5845c7359
“Anatoli <humanities5050@gmail.com>
2023-12-08 18:40:22 +0300

    feat: update js file in the main page (Fri, Dec 08, 2023 06:40:22 PM)

--- coffee-house/js/script.js
+++ coffee-house/js/script.js
@@ -1,3 +1,81 @@
 'use strict'
 
-console.log('1. Validation of pages: +18\n2.The layout matches the design: +40\n3.CSS Requirements: +10\n4.Interactivity: +32\nTotal: 100 points');
+//Burger menu
+
+const burgerIcon = document.querySelector('.burger__menu');
+const headerMenu = document.querySelector('.header__menu');
+if (burgerIcon) {
+	burgerIcon.addEventListener('click', function (e) {
+		burgerIcon.classList.toggle('_active');
+		headerMenu.classList.toggle('_active');
+		document.body.classList.toggle('_lock');
+	})
+}
+
+const menuLinks = document.querySelectorAll('.menu__item');
+menuLinks.forEach(link => {
+	link.addEventListener('click', onLinkClick)
+})
+function onLinkClick(e) {
+	if (burgerIcon.classList.contains('_active')) {
+		burgerIcon.classList.remove('_active');
+		headerMenu.classList.remove('_active');
+		document.body.classList.remove('_lock');
+	}
+}
+//Slider Favourites
+const prevBtn = document.querySelector('.favourite__arrow-left');
+const nextBtn = document.querySelector('.favourite__arrow-right');
+const slides = document.querySelectorAll('.favourite__slide');
+const btns = document.querySelectorAll('.favourite__slider-btn');
+
+let i = 0;
+
+function currentSlide(n) {
+	for (let slide of slides) {
+		slide.classList.remove('active');
+	}
+	slides[n].classList.add('active');
+}
+function currentBtn(n) {
+	for (let btn of btns) {
+		btn.classList.remove('active');
+	}
+	btns[n].classList.add('active');
+}
+function prevSlide() {
+	if (i == 0) {
+		i = slides.length - 1;
+		currentSlide(i);
+		currentBtn(i);
+	} else {
+		i--;
+		currentSlide(i);
+		currentBtn(i);
+	}
+}
+function nextSlide() {
+	if (i == slides.length - 1) {
+		i = 0;
+		currentSlide(i);
+		currentBtn(i);
+		increaseLineWidth()
+	} else {
+		i++;
+		currentSlide(i);
+		currentBtn(i);
+	}
+}
+btns.forEach(function (btn, index) {
+	btn.addEventListener('click', function () {
+		i = index;
+		currentSlide(i);
+		currentBtn(i);
+	})
+});
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+setInterval(nextSlide, 5000);
+
+
+
