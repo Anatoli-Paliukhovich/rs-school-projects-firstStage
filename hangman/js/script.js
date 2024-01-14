e622bbcd9df6e7f55c36cf0baeb7bc33db212275
“Anatoli <humanities5050@gmail.com>
2024-01-14 12:34:26 +0300

    feat: add array with hints and words in js file

---
+++ hangman/js/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const taskList = [
+  {
+    hint: "I'm black and white and loved all over the world.",
+    word: "panda",
+  },
+  {
+    hint: "I have a long neck and spots on my body.",
+    word: "giraffe",
+  },
+  {
+    hint: "I can swim, quack, and have feathers.",
+    word: "duck",
+  },
+  {
+    hint: "I'm the king of the jungle.",
+    word: "lion",
+  },
+  {
+    hint: "I have eight legs and build webs.",
+    word: "spider",
+  },
+  {
+    hint: "I live in the ocean and have a big fin on my back.",
+    word: "shark",
+  },
+  {
+    hint: "I have a bushy tail and climb trees.",
+    word: "squirrel",
+  },
+  {
+    hint: "I have a long trunk and large ears.",
+    word: "elephant",
+  },
+  {
+    hint: "I can fly and hum around flowers.",
+    word: "bee",
+  },
+  {
+    hint: "I have a shell on my back and move very slowly.",
+    word: "turtle",
+  },
+];
